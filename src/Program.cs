4f60bd0cb0e4048233cc404711665ace3bd72016
Brian <brian@briandenicola.com>
2022-09-08 01:24:01 +0000

    Dapr integration

--- src/Program.cs
+++ src/Program.cs
@@ -1,39 +1,47 @@
-using Microsoft.EntityFrameworkCore;
+//using Microsoft.EntityFrameworkCore;
+using Dapr.Client;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddDbContext<TodoDb>(opt => opt.UseInMemoryDatabase("TodoList"));
+var client = new DaprClientBuilder().Build();
+var store = "dapr-store";
+
+//builder.Services.AddDbContext<TodoDb>(opt => opt.UseInMemoryDatabase("TodoList"));
+builder.Services.AddSingleton<DaprClient>(client);
 
 var app = builder.Build();
 app.Urls.Add("http://+:5501");
 
 app.MapGet( "/", () =>  $"Hello World! The time now is {DateTime.Now}" );
 
-app.MapGet("/todos", async (TodoDb db) => await db.Todos.ToListAsync());
+//app.MapGet("/todos", async (TodoDb db) => await db.Todos.ToListAsync());
 
-app.MapGet("/todos/{id}", async (int id, TodoDb db) =>
-    await db.Todos.FindAsync(id)
+app.MapGet("/todos/{id}", async (string id, DaprClient dapr) =>
+    //await db.Todos.FindAsync(id)
+    await dapr.GetStateAsync<Todo>(store, id)
         is Todo todo
             ? Results.Ok(todo)
             : Results.NotFound()
 );
 
-app.MapPost("/todos", async (Todo todo, TodoDb db) =>
+app.MapPost("/todos", async (Todo todo, DaprClient dapr) =>
 {
-    db.Todos.Add(todo);
-    await db.SaveChangesAsync();
+    /*db.Todos.Add(todo);
+    await db.SaveChangesAsync();*/
+    await dapr.SaveStateAsync<Todo>(store, todo.Id, todo);
     return Results.Created($"/todos/{todo.Id}", todo);
 });
 
-app.MapPut("/todos/{id}", async (int id, Todo inputTodo, TodoDb db) =>
+app.MapPut("/todos/{id}", async (string id, Todo inputTodo, DaprClient dapr) =>
 {
-    var todo = await db.Todos.FindAsync(id);
+    //var todo = await db.Todos.FindAsync(id);
+    var todo = await dapr.GetStateAsync<Todo>(store, id);
 
     if (todo is null) return Results.NotFound();
 
     todo.Name = inputTodo.Name;
     todo.IsComplete = inputTodo.IsComplete;
 
-    await db.SaveChangesAsync();
+    await dapr.SaveStateAsync<Todo>(store, todo.Id, todo);
     return Results.NoContent();
 });
 
@@ -41,13 +49,13 @@ app.Run();
 
 class Todo
 {
-    public int Id { get; set; }
+    public string? Id { get; set; }
     public string? Name { get; set; }
     public bool IsComplete { get; set; }
 }
 
-class TodoDb : DbContext
+/*class TodoDb : DbContext
 {
     public TodoDb(DbContextOptions<TodoDb> options)  : base(options) { }
     public DbSet<Todo> Todos => Set<Todo>();
-}
+}*/
