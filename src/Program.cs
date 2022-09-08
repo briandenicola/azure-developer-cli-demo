dbb46c30b67947e96cd7e0163bbaad118b3b63e7
Brian <brian@briandenicola.com>
2022-09-08 04:07:37 +0000

    Cleaned up commented code

--- src/Program.cs
+++ src/Program.cs
@@ -1,11 +1,10 @@
-//using Microsoft.EntityFrameworkCore;
 using Dapr.Client;
+using Microsoft.Extensions.Logging;
 
 var builder = WebApplication.CreateBuilder(args);
 var client = new DaprClientBuilder().Build();
 var store = "dapr-state";
 
-//builder.Services.AddDbContext<TodoDb>(opt => opt.UseInMemoryDatabase("TodoList"));
 builder.Services.AddSingleton<DaprClient>(client);
 
 var app = builder.Build();
@@ -15,25 +14,26 @@ app.MapGet( "/", () =>  $"Hello World! The time now is {DateTime.Now}" );
 
 //app.MapGet("/todos", async (TodoDb db) => await db.Todos.ToListAsync());
 
-app.MapGet("/todos/{id}", async (string id, DaprClient dapr) =>
-    //await db.Todos.FindAsync(id)
-    await dapr.GetStateAsync<Todo>(store, id)
-        is Todo todo
-            ? Results.Ok(todo)
-            : Results.NotFound()
-);
+app.MapGet("/todos/{id}", async (string id,DaprClient dapr) =>
+{
+    var todo = await dapr.GetStateAsync<Todo>(store, id);
+    
+    if( todo is not null) {
+       return Results.Ok(todo);
+    }
+    
+    return Results.NotFound();
+
+});
 
 app.MapPost("/todos", async (Todo todo, DaprClient dapr) =>
 {
-    /*db.Todos.Add(todo);
-    await db.SaveChangesAsync();*/
     await dapr.SaveStateAsync<Todo>(store, todo.Id, todo);
     return Results.Created($"/todos/{todo.Id}", todo);
 });
 
 app.MapPut("/todos/{id}", async (string id, Todo inputTodo, DaprClient dapr) =>
 {
-    //var todo = await db.Todos.FindAsync(id);
     var todo = await dapr.GetStateAsync<Todo>(store, id);
 
     if (todo is null) return Results.NotFound();
@@ -53,9 +53,3 @@ class Todo
     public string? Name { get; set; }
     public bool IsComplete { get; set; }
 }
-
-/*class TodoDb : DbContext
-{
-    public TodoDb(DbContextOptions<TodoDb> options)  : base(options) { }
-    public DbSet<Todo> Todos => Set<Todo>();
-}*/
