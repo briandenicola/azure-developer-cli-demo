15dd30e43b33c954584f33dbbb54b8012a54cb17
Brian <brian@briandenicola.com>
2022-09-08 04:05:16 +0000

    Fix line spacing

--- scripts/validate.sh
+++ scripts/validate.sh
@@ -6,11 +6,9 @@ echo Health Check
 curl -w '\n' ${API_URI}/
 
 echo Post Sample Items
-curl -sX POST ${API_URI}/todos/ -d '{"Id": 123456, "Name": "Take out trash"}' -H "Content-Type: application/json" 
-curl -sX POST ${API_URI}/todos/ -d '{"Id": 7891011, "Name": "Clean your bathroom"}' -H "Content-Type: application/json" 
+curl -sX POST ${API_URI}/todos/ -d '{"Id": "123456", "Name": "Take out trash"}' -H "Content-Type: application/json" 
+curl -sX POST ${API_URI}/todos/ -d '{"Id": "7891011", "Name": "Clean your bathroom"}' -H "Content-Type: application/json" 
+echo
 
 echo Get Todo ID# 123456
 curl -s ${API_URI}/todos/123456 | jq
-
-#echo Get All Todo Items
-#curl -s ${API_URI}/todos/ | jq
