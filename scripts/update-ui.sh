6c2eadc3dbbe54cb128397971a135c013f8a818d
Brian <brian@briandenicola.com>
2023-01-18 14:33:34 +0000

    Handle dynamic paths

--- scripts/update-ui.sh
+++ scripts/update-ui.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
+SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
+SRC_PATH=$(realpath "${SCRIPT_DIR}/../src/ui/wwwroot")
+
 export $(azd env get-values | grep -i API_URI | tr -d '\"')
 export $(azd env get-values | grep -i UI_URI | tr -d '\"')
 
-cat << EOF > ../src/ui/wwwroot/appsettings.json
+cat << EOF > ${SRC_PATH}/appsettings.json
 {
     "API_URI": "${API_URI}"
 }
 EOF
 
-azd deploy --cwd .. --service ui
+azd deploy --cwd ${SCRIPT_DIR}/.. --service ui
